a4c41582de7743e7032d4ea348d144823c7c6693
John Uriel Dela Cruz <136968627+urieldc19@users.noreply.github.com>
2025-06-26 20:17:18 +0800

    Added more commands to processes

--- Scheduler/scheduler.cpp
+++ Scheduler/scheduler.cpp
@@ -5,9 +5,17 @@
 #include <process.h>
 #endif
 #include <thread>
+#include <algorithm>
 #include "scheduler.h"
 #include "PrintCommand.h"
+#include "AddCommand.h"
+#include "SubtractCommand.h"
+#include "DeclareCommand.h"
+#include "SleepCommand.h"
+#include "ForCommand.h"
 #include "config.h"
+
+#define NOMINMAX
 
 Scheduler::Scheduler(ProcessList &plist, Config& config)
     : processList(plist), numCores(config.getNumCPU()), running(false),
@@ -78,7 +86,7 @@ void Scheduler::workerThreadFunc(int coreId)
             }
             proc.setCurrentLine(i + 1); // Move to the next instruction
             processList.updateProcess(proc);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(delaysPerExec));
         }
 
         proc.setState(ProcessState::FINISHED);
@@ -95,8 +103,7 @@ void Scheduler::startBatchGeneration() {
         int processCounter = 0;
 
         while (batchGenerating) {
-
-            int insCount = minIns + rand() % (maxIns - minIns + 1);
+            int insCount = minIns + rand() % (maxIns - minIns + 1); 
 
             std::vector<std::shared_ptr<Command>> cmds;
 
@@ -105,11 +112,69 @@ void Scheduler::startBatchGeneration() {
             int pid = processList.findProcessByName(procName);
             process proc = processList.findProcess(pid);
 
-            for (int i = 0; i < insCount; ++i) {
-                proc.addInstruction(std::make_shared<PrintCommand>("Command " + std::to_string(i + 1), delaysPerExec));
+            int baseCount = insCount / 6;
+            int remaining = insCount - baseCount * 6;
+
+            int numDeclare = (baseCount > 0) ? baseCount : 1;
+            int numAdd = (baseCount > 0) ? baseCount : 1;
+            int numSubtract = (baseCount > 0) ? baseCount : 1;
+            int numPrint = (baseCount > 0) ? baseCount : 1;
+            int numSleep = (baseCount > 0) ? baseCount : 1;
+            int numFor = (remaining > 0) ? remaining : 0; 
+
+            if (remaining > 0) {
+                numFor = remaining;
+            }
+
+            // DECLARE
+            for (int i = 0; i < numDeclare; ++i) {
+                std::string varName = "var" + std::to_string(i);
+                uint16_t value = rand() % 65536;  
+                cmds.push_back(std::make_shared<DeclareCommand>(varName, value));
+            }
+
+            // ADD
+            for (int i = 0; i < numAdd; ++i) {
+                std::string var1 = "var" + std::to_string(rand() % 5);
+                std::string var2 = "var" + std::to_string(rand() % 5);
+                std::string var3 = "var" + std::to_string(rand() % 5);
+                cmds.push_back(std::make_shared<AddCommand>(var1, var2, var3));
+            }
+
+            // SUBTRACT
+            for (int i = 0; i < numSubtract; ++i) {
+                std::string var1 = "var" + std::to_string(rand() % 5);
+                std::string var2 = "var" + std::to_string(rand() % 5);
+                std::string var3 = "var" + std::to_string(rand() % 5);
+                cmds.push_back(std::make_shared<SubtractCommand>(var1, var2, var3));
+            }
+
+            // PRINT
+            for (int i = 0; i < numPrint; ++i) {
+                cmds.push_back(std::make_shared<PrintCommand>());
+            }
+
+            // SLEEP
+            for (int i = 0; i < numSleep; ++i) {
+                uint8_t sleepTime = rand() % 256;  // Sleep time between 0 and 255 (uint8)
+                cmds.push_back(std::make_shared<SleepCommand>(sleepTime));
+            }
+
+            // FOR
+            for (int i = 0; i < numFor; ++i) {
+                int repeats = 1 + rand() % 5;  // Random repeats for loop
+                std::vector<std::shared_ptr<Command>> loopInstructions = {
+                    std::make_shared<PrintCommand>()
+                };
+                cmds.push_back(std::make_shared<ForCommand>(loopInstructions, repeats));
+            }
+
+            for (const auto& cmd : cmds) {
+                proc.addInstruction(cmd);
             }
 
             addProcess(proc);
+
             std::this_thread::sleep_for(std::chrono::milliseconds(batchFreq));
         }
         });
