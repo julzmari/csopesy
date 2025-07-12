6c039d9350660bcecc994b6e0a32b393a4312f3c
xian <xianirabernardo@gmail.com>
2025-07-12 17:42:27 +0800

    fix: FOR command exceeding max-ins

--- Command/ForCommand.h
+++ Command/ForCommand.h
@@ -3,11 +3,15 @@
 #include <vector>
 #include <memory>
 
-class ForCommand : public Command {
+class ForCommand : public Command
+{
 public:
-	ForCommand(const std::vector<std::shared_ptr<Command>>& instructions, int repeats);
-	void execute(process& context) override;
+	ForCommand(const std::vector<std::shared_ptr<Command>> &instructions, int repeats);
+	void execute(process &context) override;
+	int getRepeatCount() const { return repeats; }
+	const std::vector<std::shared_ptr<Command>> &getInstructions() const { return instructions; }
 	std::shared_ptr<Command> clone() const override;
+
 public:
 	std::vector<std::shared_ptr<Command>> instructions;
 	int repeats;
